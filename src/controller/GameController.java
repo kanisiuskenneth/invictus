5d2b8c41f73a50f6312e76621029aafaa1fa83f1
emonnemo <dika.kusuma23@gmail.com>
2017-04-22 15:23:56 +0700

    Add GameView & GameController

---
+++ src/controller/GameController.java
@@ -0,0 +1,23 @@
+package controller;
+
+/**
+ * File: GameController.java
+ * Author: 13515033 - Andika Kusuma
+ */
+
+/**
+ * Kelas GameController untuk mengatur kerja game
+ */
+public class GameController {
+    /**
+     * Prosedur useItem untuk menggunakan item pada saat game
+     * @param id id item yang akan digunakan
+     */
+    public void useItem(int id){
+        // masih belum
+    }
+
+    public void reduceHP(){
+
+    }
+}
---
+++ src/view/GameView.java
@@ -0,0 +1,7 @@
+package view;
+
+/**
+ * Created by ASUS INDONESIA on 4/22/2017.
+ */
+public class GameView {
+}
