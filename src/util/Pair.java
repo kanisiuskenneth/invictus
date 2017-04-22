2efd27fbb90913ad9f1845e3b068f5e165871e4a
emonnemo <dika.kusuma23@gmail.com>
2017-04-22 12:01:52 +0700

    Add Pair Generik

---
+++ src/util/Pair.java
@@ -0,0 +1,16 @@
+package util;
+
+/**
+ * File: Pair.java
+ * Author: 13515033 - Andika Kusuma
+ */
+
+/**
+ * Kelas Pair Generik
+ * @param <K> Class first value
+ * @param <V> Class second value
+ */
+public class Pair<K,V> {
+    public K first;
+    public V second;
+}
