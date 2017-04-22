6ab0637261eec735c07dbfeb2dface3ca4020d39
Kenneth Halim <kanisiuskenneth@gmail.com>
2017-04-22 13:37:45 +0700

    Return of the Main

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import controller.ImageLoader;
+import view.*;
+
+/**
+ * Created by kennethhalim on 4/20/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+            new ImageLoader();
+            new MainFrame();
+            try {
+                Thread.sleep(5000);
+            } catch(Exception e) {
+            }
+            new Menu();
+        }
+}
