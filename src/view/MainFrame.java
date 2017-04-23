89f867087897db4e661eb0e563ac775ea2c8b0d6
keziasuhendra <keziasuhendra44@gmail.com>
2017-04-23 15:11:26 +0700

    checkstyle mainframe

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,43 +1,65 @@
 package view;
 
 import controller.ImageLoader;
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
 /**
  * Created by kennethhalim on 4/21/17.
  */
-public class MainFrame {
-    static GraphicsDevice device = GraphicsEnvironment
-            .getLocalGraphicsEnvironment().getScreenDevices()[0];
-    static int height;
-    static int width;
-    public static JFrame mainframe;
-    public static Color DARK_GRAY = new Color(30,30,30);
 
-    public MainFrame() {
-        mainframe = new JFrame("Undefeated Typer");
-        mainframe.setUndecorated(true);
-        mainframe.setIconImage(new ImageIcon("img/gamelogo.png").getImage());
-        mainframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainframe.setResizable(false);
-        width = device.getDisplayMode().getWidth();
-        height = device.getDisplayMode().getHeight();
-        System.out.println(height);
-        System.out.println(width);
-        mainframe.getContentPane().setBackground(DARK_GRAY);
-        mainframe.setSize(width,height);
-        int splash_width = widthToPx(60);
-        int splash_heiht = splash_width*ImageLoader.splash.getHeight()/ImageLoader.splash.getWidth();
-        MainFrame.mainframe.add(new JLabel(
-                new ImageIcon(ImageLoader.splash.getScaledInstance(splash_width,splash_heiht,Image.SCALE_SMOOTH))));
-        mainframe.setVisible(true);
-        device.setFullScreenWindow(mainframe);
-    }
-    static int widthToPx(int percent) {
-        return (int) percent*width/100;
-    }
-    static int heightToPx(int percent) {
-        return (int) percent*height/100;
-    }
+/**
+ * Kelas MainFrame.
+ */
+public class MainFrame {
+  static GraphicsDevice device = GraphicsEnvironment
+          .getLocalGraphicsEnvironment().getScreenDevices()[0];
+  static int height;
+  static int width;
+  public static JFrame mainframe;
+  public static Color DARK_GRAY = new Color(30, 30, 30);
+
+  /**
+   * Constructor.
+   */
+  public MainFrame() {
+    mainframe = new JFrame("Undefeated Typer");
+    mainframe.setUndecorated(true);
+    mainframe.setIconImage(new ImageIcon("img/gamelogo.png").getImage());
+    mainframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    mainframe.setResizable(false);
+    width = device.getDisplayMode().getWidth();
+    height = device.getDisplayMode().getHeight();
+    System.out.println(height);
+    System.out.println(width);
+    mainframe.getContentPane().setBackground(DARK_GRAY);
+    mainframe.setSize(width, height);
+    int splashWidth = widthToPx(60);
+    int splashHeiht = splashWidth * ImageLoader.splash.getHeight()
+            / ImageLoader.splash.getWidth();
+    MainFrame.mainframe.add(new JLabel(
+            new ImageIcon(
+                    ImageLoader.splash.getScaledInstance(
+                            splashWidth, splashHeiht, Image.SCALE_SMOOTH))));
+    mainframe.setVisible(true);
+    device.setFullScreenWindow(mainframe);
+  }
+
+  /**
+   * Mengubah persen menjadi pixels sesuai panjang layar komputer pengguna.
+   * @param percent dari panjang layar komputer pengguna.
+   * @return integer berupa pixels dari panjang layar komputer pengguna.
+   */
+  static int widthToPx(int percent) {
+    return (int) percent * width / 100;
+  }
+
+  /**
+   * Mengubah persen menjadi pixels sesuai tinggi layar komputer pengguna.
+   * @param percent dari tinggi layar komputer pengguna.
+   * @return integer berupa pixels dari tinggi layar komputer pengguna.
+   */
+  static int heightToPx(int percent) {
+    return (int) percent * height / 100;
+  }
 }
