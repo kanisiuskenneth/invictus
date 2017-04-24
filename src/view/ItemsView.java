8e9f9263fda75623ad6f739ca311c8bd988988dd
keziasuhendra <keziasuhendra44@gmail.com>
2017-04-24 14:01:36 +0700

    itemsview ditambahin money

--- src/view/ItemsView.java
+++ src/view/ItemsView.java
@@ -1,8 +1,6 @@
 package view;
 
 import controller.ImageLoader;
-import sun.applet.Main;
-
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
@@ -10,46 +8,42 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
+ * File: ItemsView.java
  * Created by 13515063 - Kezia Suhendra.
  */
 public class ItemsView {
-  Container temp;
   JPanel itemsPanel;
   static final Color GRAY = new Color(35, 35, 35);
   static final Color LIGHT_GRAY = new Color(40, 40, 40);
 
   public ItemsView() {
-    temp = MainFrame.mainframe.getContentPane();
     itemsPanel = new JPanel();
     itemsPanel.setBackground(MainFrame.DARK_GRAY);
     itemsPanel.setLayout(new BorderLayout());
     itemsPanel.setSize(MainFrame.width, MainFrame.height);
     int itemBannerWidth = 800;
-    int itemBannerHeight = ImageLoader.itemsBanner.getHeight() * itemBannerWidth/ImageLoader.itemsBanner.getWidth();
+    int itemBannerHeight = ImageLoader.itemsBanner.getHeight() * itemBannerWidth / ImageLoader.itemsBanner.getWidth();
     MainFrame.mainframe.setContentPane(itemsPanel);
     JLabel headBanner = new JLabel(new ImageIcon(
             ImageLoader.itemsBanner.getScaledInstance(itemBannerWidth, itemBannerHeight, Image.SCALE_DEFAULT)));
     itemsPanel.add(headBanner, BorderLayout.NORTH);
-
     JPanel menuPanel = new JPanel();
     menuPanel.setLayout(new GridLayout(2, 1));
     itemsPanel.add(menuPanel, BorderLayout.CENTER);
 
-    menuPanel.setBackground(Color.blue);
-
-    JPanel menuPanel3 = new JPanel();
-    menuPanel3.setLayout(new GridLayout(1,2));
-    menuPanel3.setBackground(GRAY);
-    menuPanel.add(menuPanel3);
+    JPanel menuPanel2 = new JPanel();
+    menuPanel2.setLayout(new GridLayout(1, 2));
+    menuPanel2.setBackground(GRAY);
+    menuPanel.add(menuPanel2);
 
     JPanel menuPanel1 = new JPanel();
     menuPanel1.setBackground(GRAY);
     menuPanel.add(menuPanel1);
 
-    JPanel menuPanel2 = new JPanel();
-    menuPanel2.setLayout(new GridLayout(7,1));
-    itemsPanel.add(menuPanel2, BorderLayout.EAST);
-    menuPanel2.setBackground(LIGHT_GRAY);
+    JPanel descPanel = new JPanel();
+    descPanel.setLayout(new GridLayout(7, 1));
+    itemsPanel.add(descPanel, BorderLayout.EAST);
+    descPanel.setBackground(LIGHT_GRAY);
 
     JButton slowSpell = new JButton(new ImageIcon(
             ImageLoader.slowButton.getScaledInstance(150, 150, Image.SCALE_DEFAULT)));
@@ -63,9 +57,9 @@ public class ItemsView {
             ImageLoader.shieldButton.getScaledInstance(150, 150, Image.SCALE_DEFAULT)));
     JButton dummy = new JButton(new ImageIcon(
             ImageLoader.dummyImg.getScaledInstance(195, 150, Image.SCALE_DEFAULT)));
-    menuPanel3.add(slowSpell);
-    menuPanel3.add(freezeSpell);
-    menuPanel3.add(potion);
+    menuPanel2.add(slowSpell);
+    menuPanel2.add(freezeSpell);
+    menuPanel2.add(potion);
     menuPanel1.add(lightningSpell);
     menuPanel1.add(dummy);
     menuPanel1.add(shield);
@@ -90,40 +84,40 @@ public class ItemsView {
 
     JLabel descBanner = new JLabel(new ImageIcon(
             ImageLoader.descriptionBanner.getScaledInstance(300, 50, Image.SCALE_DEFAULT)));
-    descBanner.setBorder(new EmptyBorder(0,50,0,50));
-    menuPanel2.add(descBanner, BorderLayout.NORTH);
-    JLabel name = new JLabel("",SwingConstants.CENTER);
-    menuPanel2.add(name);
-    JLabel desc = new JLabel("",SwingConstants.CENTER);
-    menuPanel2.add(desc);
+    descBanner.setBorder(new EmptyBorder(0, 50, 0, 50));
+    descPanel.add(descBanner, BorderLayout.NORTH);
+    JLabel name = new JLabel("", SwingConstants.CENTER);
+    descPanel.add(name);
+    JLabel desc = new JLabel("", SwingConstants.CENTER);
+    descPanel.add(desc);
 
     JPanel pricePanel = new JPanel();
-    menuPanel2.add(pricePanel);
+    descPanel.add(pricePanel);
     pricePanel.setBackground(LIGHT_GRAY);
     JLabel coin = new JLabel(new ImageIcon(
             ImageLoader.coin.getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
     pricePanel.add(coin);
-    JLabel price = new JLabel("",SwingConstants.CENTER);
+    JLabel price = new JLabel("", SwingConstants.CENTER);
     pricePanel.add(price);
     name.setForeground(Color.WHITE);
     desc.setForeground(Color.YELLOW);
     price.setForeground(Color.GREEN);
     name.setText("Slow Spell");
-    name.setFont(new Font("Courier New",Font.PLAIN,40));
+    name.setFont(new Font("Courier New", Font.PLAIN, 40));
     desc.setText("<html>decrease the speed <br> of the words <br> by 0.5.</html>");
-    desc.setFont(new Font("Courier New",Font.PLAIN,30));
+    desc.setFont(new Font("Courier New", Font.PLAIN, 30));
     price.setText("0");
-    price.setFont(new Font("Courier New",Font.PLAIN,30));
+    price.setFont(new Font("Courier New", Font.PLAIN, 30));
 
     JButton buy = new JButton(new ImageIcon(
             ImageLoader.buyButton.getScaledInstance(80, 40, Image.SCALE_DEFAULT)));
-    menuPanel2.add(buy);
+    descPanel.add(buy);
     buy.setBorder(BorderFactory.createEmptyBorder());
     buy.setContentAreaFilled(false);
 
     JPanel sub = new JPanel();
-    sub.setLayout(new GridLayout(1,2));
-    menuPanel2.add(sub, BorderLayout.SOUTH);
+    sub.setLayout(new GridLayout(1, 2));
+    descPanel.add(sub, BorderLayout.SOUTH);
     sub.setBackground(LIGHT_GRAY);
 
     JButton back = new JButton(new ImageIcon(
@@ -138,6 +132,19 @@ public class ItemsView {
     home.setBorder(BorderFactory.createEmptyBorder());
     home.setContentAreaFilled(false);
 
+    JPanel header = new JPanel();
+    descPanel.add(header);
+    JLabel coin1 = new JLabel(new ImageIcon(
+            ImageLoader.coin.getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
+    header.add(coin1);
+    JLabel money = new JLabel("0");
+    money.setFont(new Font("Courier New", Font.PLAIN, 30));
+    header.setBackground(LIGHT_GRAY);
+    money.setForeground(Color.GREEN);
+    header.add(money);
+
+    header.setVisible(true);
+
     menuPanel.setVisible(true);
 
     slowSpell.addActionListener(
@@ -145,11 +152,11 @@ public class ItemsView {
               @Override
               public void actionPerformed(ActionEvent e) {
                 name.setText("<html>Slow Spell<br></html>");
-                name.setFont(new Font("Courier New",Font.PLAIN,40));
+                name.setFont(new Font("Courier New", Font.PLAIN, 40));
                 desc.setText("<html>decrease the speed <br> of the words <br> by 0.5.<br></html>");
-                desc.setFont(new Font("Courier New",Font.PLAIN,30));
+                desc.setFont(new Font("Courier New", Font.PLAIN, 30));
                 price.setText("0");
-                price.setFont(new Font("Courier New",Font.PLAIN,30));
+                price.setFont(new Font("Courier New", Font.PLAIN, 30));
               }
             }
     );
@@ -158,11 +165,11 @@ public class ItemsView {
               @Override
               public void actionPerformed(ActionEvent e) {
                 name.setText("<html>Freeze Spell<br></html>");
-                name.setFont(new Font("Courier New",Font.PLAIN,40));
+                name.setFont(new Font("Courier New", Font.PLAIN, 40));
                 desc.setText("<html>stop all of the <br> words on the screen <br> for 10 seconds.<br></html>");
-                desc.setFont(new Font("Courier New",Font.PLAIN,30));
+                desc.setFont(new Font("Courier New", Font.PLAIN, 30));
                 price.setText("0");
-                price.setFont(new Font("Courier New",Font.PLAIN,30));
+                price.setFont(new Font("Courier New", Font.PLAIN, 30));
               }
             }
     );
@@ -171,11 +178,11 @@ public class ItemsView {
               @Override
               public void actionPerformed(ActionEvent e) {
                 name.setText("<html>Potion<br></html>");
-                name.setFont(new Font("Courier New",Font.PLAIN,40));
+                name.setFont(new Font("Courier New", Font.PLAIN, 40));
                 desc.setText("<html>increase the current <br> health points by <br> one heart.<br></html>");
-                desc.setFont(new Font("Courier New",Font.PLAIN,30));
+                desc.setFont(new Font("Courier New", Font.PLAIN, 30));
                 price.setText("0");
-                price.setFont(new Font("Courier New",Font.PLAIN,30));
+                price.setFont(new Font("Courier New", Font.PLAIN, 30));
               }
             }
     );
@@ -184,11 +191,11 @@ public class ItemsView {
               @Override
               public void actionPerformed(ActionEvent e) {
                 name.setText("<html>Lightning Spell<br></html>");
-                name.setFont(new Font("Courier New",Font.PLAIN,40));
+                name.setFont(new Font("Courier New", Font.PLAIN, 40));
                 desc.setText("<html>destroy all of <br> the words on <br> the screen.<br></html>");
-                desc.setFont(new Font("Courier New",Font.PLAIN,30));
+                desc.setFont(new Font("Courier New", Font.PLAIN, 30));
                 price.setText("0");
-                price.setFont(new Font("Courier New",Font.PLAIN,30));
+                price.setFont(new Font("Courier New", Font.PLAIN, 30));
               }
             }
     );
@@ -197,11 +204,11 @@ public class ItemsView {
               @Override
               public void actionPerformed(ActionEvent e) {
                 name.setText("<html>Shield<br></html>");
-                name.setFont(new Font("Courier New",Font.PLAIN,40));
+                name.setFont(new Font("Courier New", Font.PLAIN, 40));
                 desc.setText("<html>prevents the health <br> points from reducing <br> for 10 seconds.<br></html>");
-                desc.setFont(new Font("Courier New",Font.PLAIN,30));
+                desc.setFont(new Font("Courier New", Font.PLAIN, 30));
                 price.setText("0");
-                price.setFont(new Font("Courier New",Font.PLAIN,30));
+                price.setFont(new Font("Courier New", Font.PLAIN, 30));
               }
             }
     );
@@ -225,7 +232,7 @@ public class ItemsView {
             new ActionListener() {
               @Override
               public void actionPerformed(ActionEvent e) {
-                
+
               }
             }
     );
